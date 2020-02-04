2bae36ca48b47308da83f8b50ed7c49ed6eb9ec4
Anthony Risch <rischthefisch@gmail.com>
2020-02-04 03:27:14 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'pry'
 
 def my_find(collection)
@@ -14,3 +15,17 @@ end
 #     i = i + 1
 #   end
 # end
+=======
+require 'pry'
+
+def my_find(collection)
+ i = 0 
+  while i < 100
+  yield(collection[i]) if
+  return collection[i]
+   i = i + 1 
+ end
+end
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+>>>>>>> 7bc8eb5231567aa142978da2619f6e18976c012b
